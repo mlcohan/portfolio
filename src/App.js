8f7df033ff16bc054a38df135a9b7571f6d83099
Mary Cohan <mary.cohan@gmail.com>
2021-05-13 18:46:36 -0700

    get up App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import Project from './components/Project'
+import Projects from './components/Projects';
+import Home from "./components/pages/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Navbar/>
-      <Project/>
+    <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+    </div>
+    <div>
       <Footer/>
     </div>
+    </Router>
   );
 }
 
